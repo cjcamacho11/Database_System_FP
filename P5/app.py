b022d606ed1e90657731ea6fb4741805c605c37c
Christian Camacho <102244830+cjcamacho11@users.noreply.github.com>
2024-12-02 12:18:19 -0500

    Update app.py

--- P5/app.py
+++ P5/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from user_routes import api  
+from playlist_routes import playlist_routes
 
 app = Flask(__name__)
 
 app.register_blueprint(api, url_prefix="/api")  
+app.register_blueprint(playlist_routes, url_prefix="/api/playlists")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
