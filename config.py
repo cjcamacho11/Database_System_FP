687b4313d82e2f3acbcbaea842f1c92ec01e713d
mikasade <67707752+mikasade@users.noreply.github.com>
2024-12-01 23:07:48 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+DB_CONFIG = {
+    "DRIVER": "ODBC Driver 17 for SQL Server",
+    "SERVER": "localhost",
+    "DATABASE": "MusicMedia",
+    "UID": "SA",
+    "PWD": "YourPassword",
+    "Timeout": 60
+}
