45ab84a72191b88534c2d885f7914b9d0a73a7bd
Dylanandersen35 <112601721+Dylanandersen35@users.noreply.github.com>
2024-12-02 21:22:36 -0500

    Update app.py

--- P5/app.py
+++ P5/app.py
@@ -3,6 +3,7 @@ from user_routes import api
 from playlist_routes import playlist_routes
 from artist_routes import artist_routes
 from song_routes import song_routes
+from albums_routes import albums_routes
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app.register_blueprint(api, url_prefix="/api")
 app.register_blueprint(playlist_routes, url_prefix="/api/playlists")
 app.register_blueprint(artist_routes, url_prefix="/api/artists")
 app.register_blueprint(song_routes, url_prefix="/api/songs")
+app.register_blueprint(albums_routes, url_prefix="/api/albums")
 
 
 if __name__ == "__main__":
